b55433c6a2d95229ce4e8b962672bff0899d7f38
Alen <alechkr@gmail.com>
2020-08-09 14:18:26 +0200

    input data saved to the cities array

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ app.listen(PORT, (err) => {
 // Views path for finding the EJS templates
 app.set('views', path.join(__dirname, '/client/views'));
 
+// Array in which data inputed by the user is saved
+let cities = [
+    {city: 'city', country: 'country', image: 'https://cdn.pixabay.com/photo/2013/01/13/21/48/eiger-74848_960_720.jpg'},
+    {city: 'city2', country: 'USA', image: 'https://cdn.pixabay.com/photo/2016/12/15/07/54/horseshoe-bend-1908283__340.jpg'}
+];
+
 // Test route
 app.get('/test', (req, res) => {         
     res.send('express is working');
@@ -33,10 +39,6 @@ app.get('/', (req, res) => {
 
 // Route for displaying cities
 app.get('/cities', (req, res) => {
-    let cities = [
-        {city: 'city', country: 'country', description: 'description 1', img: 'https://cdn.pixabay.com/photo/2013/01/13/21/48/eiger-74848_960_720.jpg'},
-        {city: 'city2', country: 'USA', description: 'description 2', img: 'https://cdn.pixabay.com/photo/2016/12/15/07/54/horseshoe-bend-1908283__340.jpg'}
-    ];
     res.render('cities.ejs', {cities});
 });
 
@@ -45,9 +47,21 @@ app.get('/cities/new', (req, res) => {
     res.render('new.ejs');
 });
 
-// Route to post the city
+// Route to post the city to the server and save it to the cities array
 app.post('/cities', (req, res) => {
+    let city = req.body.city;
+    let country = req.body.country;
+    let image = req.body.image;
+
+    cities.push({
+        "city": city,
+        "country": country,
+        "image": image
+        });
+
+    console.log(cities)
     res.send('POST is working');
+
 });
 
 
