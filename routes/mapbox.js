c798bfdf32e2a796d47ef9c487f66be64d88faf1
Alen <alechkr@gmail.com>
2021-01-01 18:38:45 +0100

    display latitude and longitude on show.ejs

--- routes/mapbox.js
+++ routes/mapbox.js
@@ -1,9 +1,3 @@
-/* if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-dotenv = require('dotenv').config();
- */
-
 const express = require('express');
 const router = express.Router();
 
