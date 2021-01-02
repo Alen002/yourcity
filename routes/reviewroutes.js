9fb4b1ae972d427956546fdd5d9ccbb3d443d821
Alen <alechkr@gmail.com>
2021-01-02 19:01:22 +0100

    implemented post route for saving the review data to the database

--- routes/reviewroutes.js
+++ routes/reviewroutes.js
@@ -3,19 +3,35 @@ const router = express.Router();
 
 // Import mongodb models
 const City = require('../models/city');
-const Comment = require('../models/comment');
+const Review = require('../models/review');
 const User = require('../models/user');
 
+// CREATE - Create a new review and push it to the related city
+router.post('/cities/:id/reviews', /* isLoggedIn, */ async (req, res) => {
+  const review = new Review ({
+      review: req.sanitize(req.body.review),
+      rating: req.sanitize(req.body.rating)
+  });
+  review.save();
+  try {
+      const cities = await City.findById(req.params.id);
+      cities.reviews.push(review); 
+      cities.save();
+       
+      res.redirect('/cities');
+  } catch(err) {
+      res.send('Something went wrong while trying to save the comment to the db');
+  }
+}); 
 
-router.post('/cities/:id/reviews', (req, res) => {
-   
-   
+// Just for testing purposes
+/* router.post('/cities/:id/reviews', (req, res) => {   
   let object = {
       review: req.body.review,
       rating: req.body.rating,
   }
   res.send(object);
-});
+}); */
 
 module.exports = router;
 
